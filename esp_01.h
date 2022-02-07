96a8850d5f5653ac2a03d59e89b4f52ea4c49726
ErdoganDokmeci <49209180+ErdoganDokmeci@users.noreply.github.com>
2022-02-07 10:53:30 +0300

    Update esp_01.h

--- esp_01.h
+++ esp_01.h
@@ -3,12 +3,12 @@
 #define MAIN_BUFFER_SIZE 	256
 #define WIFI_SSID 		"xxxxxxxxxxxxx"
 #define WIFI_PSWD 		"xxxxxxxxx"
-#define MAIL_ID				"xxxxxxxxx"
-#define MAIL_PSWD			"xxxxxxxxx"
-#define MAIL_FROM			"xxxxxxxxx"
-#define MAIL_TO				"xxxxxxxxx"
-#define MAIL_SUBJ			"Sending e-mail using STM32F4"
-#define MAIL_BODY			"Chance favors the prepared mind, Louis Pasteur."
+#define MAIL_ID			"xxxxxxxxx"
+#define MAIL_PSWD		"xxxxxxxxx"
+#define MAIL_FROM		"xxxxxxxxx"
+#define MAIL_TO			"xxxxxxxxx"
+#define MAIL_SUBJ		"Sending e-mail using STM32F4"
+#define MAIL_BODY		"Chance favors the prepared mind, Louis Pasteur."
 
 extern uint8_t main_buffer[MAIN_BUFFER_SIZE];
 
