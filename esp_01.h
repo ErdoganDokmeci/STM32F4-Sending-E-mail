63fc24f401a1bab846f06c9d22f7780b13aaab23
ErdoganDokmeci <49209180+ErdoganDokmeci@users.noreply.github.com>
2022-02-07 10:45:13 +0300

    Update esp_01.h

--- esp_01.h
+++ esp_01.h
@@ -3,14 +3,14 @@
 
 
 #define MAIN_BUFFER_SIZE 	256
-#define WIFI_SSID 				"TURKSAT-KABLONET-09EF-2.4G"
-#define WIFI_PSWD 				"91fd704fz"
-#define MAIL_ID						"ZXJkb2dhbi5kb2ttZWNpQGVtby5vcmcudHI="
-#define MAIL_PSWD					"U2V2dmFsNjc2MDA="
-#define MAIL_FROM					"erdogan.dokmeci@emo.org.tr"
-#define MAIL_TO						"erdogan_dokmeci@yahoo.com"
-#define MAIL_SUBJ			"Sending e-mail using STM32F4"
-#define MAIL_BODY					"Chance favors the prepared mind Louis Pasteur."
+#define WIFI_SSID 				"xxxxxxxxxxxxx"
+#define WIFI_PSWD 				"xxxxxxxxx"
+#define MAIL_ID						"xxxxxxxxx"
+#define MAIL_PSWD					"xxxxxxxxx"
+#define MAIL_FROM					"xxxxxxxxx"
+#define MAIL_TO						"xxxxxxxxx"
+#define MAIL_SUBJ			    "Sending e-mail using STM32F4"
+#define MAIL_BODY					"Chance favors the prepared mind, Louis Pasteur."
 
 extern uint8_t main_buffer[MAIN_BUFFER_SIZE];
 
